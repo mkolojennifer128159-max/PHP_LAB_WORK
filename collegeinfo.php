d7ab131f8018361aa57ac5681c63b0b99e3f543e
Jennifer <mkolojennifer.128159@marwadiuniversity.ac.in>
2026-07-14 10:50:26 +0530

    Add files via upload

---
+++ collegeinfo.php
@@ -0,0 +1,10 @@
+<?php
+function collegeInfo()
+{
+    echo "College Name: Marwadi University<br>";
+    echo "Course Name: BCA <br>";
+    echo "Academic Year: 2025-2026";
+}
+
+collegeInfo(); 
+?>
